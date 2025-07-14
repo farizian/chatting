0ac61f28e25a143cf184ffcc56abff420baccc8f
@farizian <farizirfan8@gmail.com>
2025-07-14 20:05:14 +0700

    Fix WebSocket connection by updating socket configuration

--- src/config/socket.js
+++ src/config/socket.js
@@ -2,9 +2,11 @@ import { io } from "socket.io-client";
 import { API_URL } from "../helper/env";
 
 const socket = io(API_URL, {
-  transports: ['websocket', 'polling'],
+  transports: ['polling', 'websocket'],
   timeout: 20000,
-  forceNew: true
+  forceNew: true,
+  upgrade: true,
+  rememberUpgrade: false
 });
 
 export default socket;

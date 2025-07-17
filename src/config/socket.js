84621c311418d1e16578776b48329a943cec886d
@farizian <farizirfan8@gmail.com>
2025-07-17 11:15:04 +0700

    Setup aplikasi chat untuk development local

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { API_URL } from "../helper/env";
 
-const socket = io('https://chattingbe.farizian.my.id', {
+const socket = io('http://localhost:5000', {
   transports: ['polling', 'websocket'],
   timeout: 20000,
   forceNew: true,
